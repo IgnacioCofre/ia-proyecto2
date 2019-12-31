fffadcbdf162be48fcb39f4bef7970ff9f7a1e3e
Ignacio <ignacio.cofre.14@sansano.usm.cl>
2019-12-30 22:49:02 -0300

    cambios al parseo de los casos

--- mainESSp.cpp
+++ mainESSp.cpp
@@ -2,36 +2,69 @@
 #include <iostream>
 #include <string>
 #include <bits/stdc++.h>
+#include <regex>
 
 using namespace std;
  
 int main () {
 
 	string str;
+	/* nombre de archivo por defecto */
 	string file_name = "caso1.txt"; 
+	/* casos de prueba deben estar en la carpeta "Casos" */
 	std::ifstream file("Casos//"+file_name);
 
 	if(file.fail()){
-		cout << "caso no encontrado\n";
+		cout << "caso de prueba no encontrado\n";
 	
 	} else {
-		list<string> list1;
+
+		/* lista con el archivo */
+		list<string> list_aux;
 
 		while (std::getline(file,str))
 		{
-			list1.push_back(str);
-		}
+			if(!std::regex_match (str, std::regex("(^#[\\s\\S]*)")) && str != "") {
+				list_aux.push_back(str);
+			}
 
-		list<string>::iterator line;
-		for(line = list1.begin(); line !=list1.end(); line++)
-		{
-			cout<< *line <<"\n";
 		}
 
 		file.close();
+		
+		/*	Iteracion sobre lineas de texto en lista */
+
+		// list<string>::iterator line;
+		// for(line = list_aux.begin(); line !=list_aux.end(); line++)
+		// {
+		// 	cout<< *line <<"\n";
+		// }
+
+		/* Parseo de datos sobre la lista auxiliar*/
+
+		string HORIZON = "SECTION_HORIZON";
+		string SHIFTS = "SECTION_SHIFTS";
+		string STAFF = "SECTION_STAFF";
+		string DAY_OFF = "SECTION_DAY_OFF";
+		string SHIFT_ON_REQUESTS = "SECTION_SHIFT_ON_REQUESTS";
+		string SHIFT_OFF_REQUESTS = "SECTION_SHIFT_OFF_REQUESTS";
+		string COVER = "SECTION_COVER";
+
+		int Horizon;
+		list<string> Shifts_list;
+		list<string> Staff_list;
+		list<string> Days_off_list;
+		list<string> Shift_on_request_list;
+		list<string> Shift_off_request_list;
+		list<string> Cover_list;
+		list<string>::iterator line;
+
+		for(line = list_aux.begin(); *line != SHIFTS; line++)
+		{
+			Horizon = std::stoi(*line);
+		}
+		
 	}
-
-
 
     return 0;
 }
